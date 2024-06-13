2e1a290eaf72ac7c31b6f36737e598b8f009a889
JesusMunnoz <jesusmunozpost@gmail.com>
2024-06-13 19:23:57 +0200

    t3.1 3.2 y 3.3

---
+++ tema3.3/src/routers/book.routers.js
@@ -0,0 +1,13 @@
+const { Router } = require ("express")
+const router = Router();
+const booksCtrl = require("../controller/book.controller")
+
+router.get("/", booksCtrl.getBook);
+
+router.post("/", booksCtrl.createBook);
+
+router.put("/:id", booksCtrl.updateBook);
+
+router.delete("/:id", booksCtrl.deleteBook);
+
+module.exports = router;
