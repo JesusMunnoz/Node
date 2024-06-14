a372634db5c6772c266675e9e537c4d525a546c3
JesusMunnoz <jesusmunozpost@gmail.com>
2024-06-14 16:19:00 +0200

    t3.2

--- tema3.2/src/routers/book.routers.js
+++ tema3.2/src/routers/book.routers.js
@@ -11,3 +11,35 @@ router.put("/:id", booksCtrl.updateBook);
 router.delete("/:id", booksCtrl.deleteBook);
 
 module.exports = router;
+
+/*
+{
+  "title": "The Hobbit ",
+  "type": "kindle",
+  "author": "J.R.R. Tolkien",
+  "price": 10,
+  "photo": "https://www.planetadelibros.com/usuaris/libros/fotos/357/original/portada_el-hobbit_j-r-r-tolkien_202207271130.jpg",
+  "id_book": 1,
+  "id_user": 1
+}
+
+{
+  "title": "Lord of the rings ",
+  "type": "kindle",
+  "author": "J.R.R. Tolkien",
+  "price": 10,
+  "photo": "picture",
+  "id_book": 2,
+  "id_user": 1
+}
+
+{
+  "title": "The Hobbit -extended",
+  "type": "kindle",
+  "author": "J.R.R. Tolkien",
+  "price": 105,
+  "photo": "picture",
+  "id_book": 3,
+  "id_user": 1
+}
+*/
